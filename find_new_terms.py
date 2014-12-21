6018135af4bb27665dc45cfdc321b849a8917926
Chris Natoli <chrisnatoli@gmail.com>
2014-12-21 18:28:21 -0500

    sixth version of find_new_terms, using CountVectorizer from scikit-learn, which is a 2d array with documents as rows, words as columns, and word counts as the cell entries

--- find_new_terms.py
+++ find_new_terms.py
@@ -1,127 +1,85 @@
 import csv
 import os
-import sys
-import gc
-import string
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
-print('start')
-
-# To conserve memory, this is a generator that effectively does the
-# same job as a string's split function with whitespace as delimiter.
-def split_generator(s):
-    word = ''
-    for char in s:
-        if char in string.whitespace:
-            yield word
-            word = ''
-        else:
-            word = word + char
-
-directory = '../klab/monthly_abstracts-todo/'
+directory = 'monthly_abstracts/'
 filenames = sorted([ f for f in os.listdir(directory) ])
+fullpaths = [ directory+f for f in filenames ]
+dates = sorted([ (int(filename[ : len('YYYY')]),
+                  int(filename[len('YYYY-') : -len('.txt')]))
+                 for filename in filenames ])
 
-# Create a dictionary mapping from the date to a string containing the
-# entire text for that date. Along the way, accumulate a set of all
-# the words. I chose to store the entire text in a string rather than
-# in a list of words (or a dictionary from word to word frequency)
-# to save memory.
-dates = []
-all_words = set()
-monthly_text = dict()
-for filename in filenames:
-    #gc.collect()
+def preprocess(text):
+    text = text.lower()
+    punctuation = (',','"',"'",'.','(',')',':','--','---',
+                   '#','[',']','{','}','!','?','$','%',';')
+    for p in punctuation:
+        text = text.replace(p,' {} '.format(p))
+    return text
 
-    date = (int(filename[ :4]), int(filename[5:-4]))
-    dates.append(date)
+def tokenize(text):
+    return text.split()
 
-    print(date)
+vectorizer = CountVectorizer(input='filename',
+                             preprocessor=preprocess,
+                             tokenizer=tokenize)
+counts = vectorizer.fit_transform(fullpaths).toarray()
+all_words = vectorizer.get_feature_names()
 
-    with open(directory+filename) as fp:
-        abstracts = fp.read().lower()
-        punctuation = (',','"',"'",'.','(',')',':','--','---',
-                       '#','[',']','{','}','!','?','$','%')
-        for p in punctuation:
-            abstracts = abstracts.replace(p,' {} '.format(p))
-        monthly_text[date] = abstracts
+print('CORPUS IS VECTORIZED')
 
-        words = abstracts.split()
-        all_words.update(set(words))
-
-print('FINISHED READING IN DATA')
-
-dates.sort()
-all_words = sorted(list(all_words))
-
-print(len(all_words))
-
-# Search for new words by filtering out the candidates in all_words.
-# The dates range from start_date (inclusive) to end_date (exclusive).
-# When a new word is found, add it and its data to the table.
 start_date = (1970,1)
 end_date = (2100,1)
 min_freq = 50
 min_density = 0.8
-header = ['word', 'first appearance', 'term frequency', 'num months']
+header = ['word', 'first appearance', 'term freqency', 'num months']
 table = [header]
+# For every word in the corpus, check that it passes a few filters in
+# order to be considered a new word. If it does, add it to the table
+# along with some information about it.
 for word in all_words:
     print(word)
-    # The first filter is that a word is new if it is not seen in
-    # previous months. This is implemented by finding the date of
-    # first appearance and checking if that date is before the defined
-    # start_date, after the end_date, and isn't the last date.
-    first_appearance = None # This is just a flag to control the first forloop.
+
+    # First filter:
+    # Word does not appear before start_date.
     for date in dates:
-        if first_appearance is not None:
+        if counts[dates.index(date)][all_words.index(word)] != 0:
+            first_appearance = date
             break
-        for w in split_generator(monthly_text[date]):
-            if w == word:
-                first_appearance = date
-                break
-    if (first_appearance is None
-        or first_appearance <= start_date
+    if (first_appearance <= start_date
         or first_appearance > end_date
         or first_appearance == dates[-1]):
         continue
 
-    # Run the next two filters simultaneously to save time:
-    # The second filter is that a word must attain a minimum frequency
-    # across the entirety of text in the following months. Trying min
-    # of 25 to 50.
-    # The third filter is that the word must occur in a minimum number
-    # of months out of the following months (i.e., "density").
-    # Shooting for densities much higher than 50%.
-    raw_freq = 0
-    num_months = 0
-    for date in dates:
-        if date <= first_appearance:
-            continue
-        word_in_month = False
-        for w in split_generator(monthly_text[date]):
-            if w == word:
-                raw_freq = raw_freq + 1
-                word_in_month = True
-        if word_in_month:
-            num_months = num_months + 1
     # Second filter:
-    '''
-    if raw_freq <= min_freq:
-        continue
-    '''
-    # Third filter
+    # Word occurs at least min_freq times after its first appearance.
+    raw_freq = sum([ counts[dates.index(date)][all_words.index(word)]
+                     for date in dates if date > first_appearance ])
+    #if raw_freq <= min_freq:
+    #    continue
+
+    # Third filter:
+    # Word occurs in at least x% of the months following its first
+    # appearance, where x% = min_density.
+    num_months = len([ date for date in dates
+                       if (date > first_appearance and
+                           counts[dates.index(date)][all_words.index(word)] > 0) ])
     total = len(dates) - dates.index(first_appearance) - 1
     density = num_months / total
     if density <= min_density:
         continue
-
-    # If the word passes the filters, then add it to the table.
+    
+    # If the word passes all the filters, then add it to the table.
     # The header of the table is as follows: word, date of first
     # appearance, raw term frequency after first appearance, number of
     # months it occurs in after the first appearance.
     table.append([ word, '{}-{}'.format(date[0],date[1]),
                    raw_freq, num_months ])
 
+
 # Compose the printout for testing:
-new_words = [ row[0] for row in table[1: ] ]
+new_words = [ row[0] for row in table ]
 printout = '''min_density = {}
 start_date = {}
 end_date = {}
