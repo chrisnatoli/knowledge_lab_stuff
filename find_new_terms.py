a8aca2d98d048cad5750b67580ec9a1790ceaffa
Chris Natoli <chrisnatoli@gmail.com>
2014-12-21 18:35:13 -0500

    ninth version of find_new_terms: reads text into sets from the beginning (rather than splitting strings again and again when comparing documents) to save time; also, edits allow for less-than-100-percent density

--- find_new_terms.py
+++ find_new_terms.py
@@ -13,8 +13,8 @@ monthly_text = dict()
 for filename in filenames:
     start_time = datetime.now()
 
-    date =  (int(filename[ : len('YYYY')]),
-             int(filename[len('YYYY-') : -len('.txt')]))
+    date = (int(filename[ : len('YYYY')]),
+            int(filename[len('YYYY-') : -len('.txt')]))
 
     with open(directory+filename) as fp:
         text = fp.read().lower()
@@ -22,7 +22,7 @@ for filename in filenames:
                        '#','[',']','{','}','!','?','$','%',';')
         for p in punctuation:
             text = text.replace(p,' {} '.format(p))
-        monthly_text[date] = text
+        monthly_text[date] = set(text.split())
 
     print('Text for {} was read in {}'.format(str(date),
                                               str(datetime.now()-start_time)))
@@ -31,11 +31,14 @@ dates = sorted(monthly_text.keys())
 
 print('All text was read in ' + str(datetime.now() - the_beginning))
 
+exit()
+
 # Consider each month's text as a set of candidate new words. Subtract
 # from this set all words in previous months. Intersect the remainder
 # with all future months.
-start_date = (1960,1)#(2007,3)#
-end_date = (2005,1)#(2009,1)#
+min_density = 0.9
+start_date = (1970,1)#(2007,3)#
+end_date = (2000,1)#(2009,1)#
 new_words = set()
 for date in [ d for d in dates if start_date <= d < end_date ]:
     print('\nConsidering the candidate set of words from '+str(date))
@@ -53,13 +56,23 @@ for date in [ d for d in dates if start_date <= d < end_date ]:
         print('This loop took '+str(datetime.now() - start_time))
         continue
 
-    # Intersect the candidates with all words in the following months,
-    # to ensure that the candidates are at least somewhat important terms.
+    # Rather than intersect the candidates with /every/ following
+    # month, consider the candidates that are in some but not all of
+    # the following months. If a candidate misses too many months (as
+    # defined by min_density), then it loses candidacy. Any that
+    # remain after comparing with every following month are considered
+    # relevant enough to the literature to be new words.
+    num_misses = { word:0 for word in candidates }
+    max_misses = int((1-min_density) * len([ d for d in dates if d > date ]))
     for future_date in [ d for d in dates if d > date ]:
-        candidates = candidates.intersection(
-            set(monthly_text[future_date].split()))
+        diff = candidates.difference(set(monthly_text[future_date].split()))
+        for word in diff:
+            num_misses[word] = num_misses[word] + 1
+        for word in candidates.copy():
+            if num_misses[word] > max_misses:
+                candidates.remove(word)
         if not candidates:
-            print('No candidates left after intersecting with {}'.format(
+            print('No candidates left after comparing with {}'.format(
                     future_date))
             print('This loop took '+str(datetime.now() - start_time))
             break
