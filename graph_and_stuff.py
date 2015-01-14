8f385032d10abe2f4e1082055d90bd42ddb7aceb
Chris Natoli <chrisnatoli@gmail.com>
2015-01-13 20:19:46 -0600

    prepared graph_and_stuff.py to make plots about old (i.e., not new) words when i finish collecting data about old words; graph_and_stuff now also makes a scatterplot in which points are colored by year of first appearance, showing a very noticeable trend in which words that were invented more recently have higher mean KL but lower stddev; also cleaned up some old plots

--- graph_and_stuff.py
+++ graph_and_stuff.py
@@ -3,90 +3,113 @@ import numpy as np
 import matplotlib.pylab as plt
 
 
-# Plot a bar graph of the number of new words each year.
 
-input_filename = 'new_words.csv'
+KLs_filename = '_word_symKL_scores.txt'
 
-def string_to_date(date):
-    return (int(date[ : len('YYYY')]), int(date[len('YYYY-') : ]))
-
-with open(input_filename) as fp:
-    table = [ row for row in csv.reader(fp, delimiter=',') ]
-
-dates = sorted([ (year, month) for year in range(1970,2000)
-                 for month in range(1,13) ])
-first_appearances = [ string_to_date(row[1]) for row in table[1:] ]
-word_counts = [ first_appearances.count(date) for date in dates ]
-
-rang = range(0,len(dates),12)
-ticks = [ '{}-{}'.format(dates[i][0], dates[i][1]) for i in rang ]
-
-plt.bar(range(len(dates)), word_counts)
-plt.xticks(rang, ticks, rotation='vertical')
-plt.savefig('new_words_per_year.png', dpi=300)
-plt.close()
-
-
-
-# Plot three histograms:
+# For both old and new words, plot three histograms:
 # - one of all KL scores for all new words,
 # - one of the mean KL score for each new word, and
 # - another of stddev of the KL scores for each new word.
-# Also make a scatterplot of tuples (mean, stddev) for each word.
-input_filename = 'new_word_KLcotf_scores.txt'
+#oldnew = ('old','new')
+oldnew = ['new']
 kl_scores = dict()
-with open(input_filename) as fp:
-    for line in fp:
-        tokens = line.strip().split(',')
-        word = tokens[0]
-        if tokens[1] == '':
-            continue
-        scores = list(map(float, tokens[1:]))
-        kl_scores[word] = scores
+flat_kl_scores = dict()
+kl_means = dict()
+kl_stddevs = dict()
+for when in oldnew:
+    kl_scores[when] = dict()
+    with open(when + KLs_filename) as fp:
+        for line in fp:
+            tokens = line.strip().split(',')
+            word = tokens[0]
+            if tokens[1] == '':
+                continue
+            scores = list(map(float, tokens[1:]))
+            if scores != []:
+                kl_scores[when][word] = scores
+    
+    flat_kl_scores[when] = []
+    for word in kl_scores[when].keys():
+        flat_kl_scores[when].extend(kl_scores[when][word])
+    plt.hist(flat_kl_scores[when], bins=50)
+    plt.savefig('plots/hist_of_{}_symKL_scores.png'.format(when))
+    plt.close()
+    
+    kl_means[when] = { word : np.mean(scores) for (word,scores)
+                      in kl_scores[when].items() }
+    plt.hist(list(kl_means[when].values()), bins=50)
+    plt.savefig('plots/hist_of_{}_symKL_means.png'.format(when))
+    plt.close()
+    
+    kl_stddevs[when] = { word : np.std(scores) for (word,scores)
+                         in kl_scores[when].items() }
+    plt.hist(list(kl_stddevs[when].values()), bins=50)
+    plt.savefig('plots/hist_of_{}_symKL_stddevs.png'.format(when))
+    plt.close()
 
-kls = []
-for word in kl_scores.keys():
-    kls.extend(kl_scores[word])
-plt.hist(kls, bins=50)
-plt.savefig('plots/hist_of_KLcotf_scores.png')
-plt.close()
 
-kl_avgs = { word : np.mean(scores) for (word,scores) in kl_scores.items()
-            if scores != [] }
-plt.hist(list(kl_avgs.values()), bins=50)
-plt.savefig('plots/hist_of_KLcotf_avgs.png')
-plt.close()
 
-kl_stddevs = { word : np.std(scores) for (word,scores) in kl_scores.items() 
-               if scores != [] }
-plt.hist(list(kl_stddevs.values()), bins=50)
-plt.savefig('plots/hist_of_KLcotf_stddevs.png')
-plt.close()
-
-xs = [ kl_avgs[word] for word in kl_scores.keys() if kl_scores[word] != [] ]
-ys = [ kl_stddevs[word] for word in kl_scores.keys() if kl_scores[word] != [] ]
-plt.scatter(xs,ys,s=1)
+# Make a single scatterplot of tuples (mean, stddev) for
+# both old and new word.
+for when in oldnew:
+    xs = [ kl_means[when][word] for word in kl_scores[when].keys() ]
+    ys = [ kl_stddevs[when][word] for word in kl_scores[when].keys() ]
+    c = 'green' if when == 'old' else 'blue'
+    plt.scatter(xs, ys, s=1, color=c)
 plt.xlabel('Mean KL score of a given word over time')
 plt.ylabel('Standard deviation of KL scores for a given word over time')
-plt.savefig('plots/scatter_KLcotf_mean_vs_std.png')
+plt.savefig('plots/scatter_symKL_mean_vs_std_old_and_new.png')
 plt.close()
+
+
+
+# Make another scatterplot of tuples 9mean, stddev) for only new
+# words, where the point is colored by the year of the word's
+# first appearance.
+def string_to_date(date):
+    return (int(date[ : len('YYYY')]), int(date[len('YYYY-') : ]))
+    
+new_words_filename = 'new_words.csv'
+with open(new_words_filename) as fp:
+    reader = csv.reader(fp, delimiter=',')
+    header = next(reader)
+    table = [ row for row in csv.reader(fp, delimiter=',') ]
+
+first_appearances = { row[header.index('word')] :
+                        string_to_date(row[header.index('first appearance')])
+                      for row in table }
+
+xs = [ kl_means['new'][word] for word in kl_scores['new'].keys() ]
+ys = [ kl_stddevs['new'][word] for word in kl_scores['new'].keys() ]
+cs = [ plt.cm.coolwarm((first_appearances[word][0]-1970)/(2000-1970))
+       for word in kl_scores['new'].keys() ]
+plt.scatter(xs, ys, s=1, color=cs)
+plt.xlabel('Mean KL score of a given word over time')
+plt.ylabel('Standard deviation of KL scores for a given word over time')
+plt.savefig('plots/scatter_symKL_mean_vs_std_with_yr.png')
+plt.close()
+
+
+
 
 
 
 # And then do some other stuff:
 
-# Dump the scores, means, and stddevs into plaintext files so
+# Dump the scores, means, and stddevs for new words into plaintext files so
 # they'll be easy to read into R.
 output_directory = 'tmp/'
 output_filenames = ['all_KLs','all_KL_means','all_KL_stddevs']
-output_data = [kls, kl_avgs.values(), kl_stddevs.values() ]
+output_data = [flat_kl_scores['new'],
+               kl_means['new'].values(),
+               kl_stddevs['new'].values() ]
 for i,filename in enumerate(output_filenames):
     with open(output_directory + filename, 'w') as fp:
         fp.write('\n'.join([ str(x) for x in output_data[i] ]))
 
 # Use a normal distribution to sample words from the two peaks of the
-# bimodal distribution of words.
-kl_avgs = sorted([ (avg,word) for (word,avg) in kl_avgs.items() ])
+# bimodal distribution of new words.
+kl_means = sorted([ (avg,word) for (word,avg) in kl_means['new'].items() ])
 left_peak = 1.9
 right_peak = 2.1
 stddev = 0.03
@@ -97,7 +120,7 @@ right_words = set()
 for peak in (left_peak, right_peak):
     for i in range(num_samples):
         r = np.random.randn() * stddev + peak
-        for (avg,word) in kl_avgs:
+        for (avg,word) in kl_means:
             if avg > r:
                 if peak == left_peak:
                     left_words.add(word)
