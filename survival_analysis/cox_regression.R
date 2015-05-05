a062ff56ac6cad69ddb4d9a3a6db4a8c338eb426
Chris Natoli <chrisnatoli@gmail.com>
2015-05-04 22:01:46 -0500

    edited cox_regression.R to incorporate tfidf as a covariate

--- survival_analysis/cox_regression.R
+++ survival_analysis/cox_regression.R
@@ -1,6 +1,6 @@
 library("survival")
 
-dead.words <- read.csv(file="dead_words_with_kl_interpolated.csv",
+dead.words <- read.csv(file="dead_words_with_covariates_interpolated.csv",
                        head=TRUE, sep=",")
-fit <- coxph(Surv(time1, time2, status) ~ kl.score, data=dead.words)
+fit <- coxph(Surv(time1, time2, status) ~ kl.score + tfidf, data=dead.words)
 print(fit)
